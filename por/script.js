de7730bbd6b07fa4b850d7b2e2c48019dab7e48f
Richard <66733313+Richard-Gabriel68@users.noreply.github.com>
2021-06-18 17:47:21 -0300

    Add files via upload

--- por/script.js
+++ por/script.js
@@ -7,17 +7,20 @@ let menu =
         {
             this.showing = true;
             document.querySelector("aside").style.right="0";
+            document.querySelector('.shadow').style.display='block';
         }
         else
         {
             this.showing = false;
             document.querySelector("aside").style.right="-40vw";
+            document.querySelector('.shadow').style.display='none';
         }
     },
     direcionar: function()
     {
         document.querySelector("aside").style.right="-40vw";
         this.showing = false;
+        document.querySelector('.shadow').style.display='none';
     }
 };
 
@@ -26,4 +29,6 @@ function seta()
     document.querySelector(".ArrowArea").style.display="none";
 }
 
+
 window.addEventListener("scroll", seta);
+
